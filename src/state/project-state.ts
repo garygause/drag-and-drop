5ac838fd526c1cc23432e315a79973b30894da82
Gary Gause <garygause@gmail.com>
2024-01-09 15:45:40 -0800

    setup webpack

--- src/state/project-state.ts
+++ src/state/project-state.ts
@@ -1,4 +1,4 @@
-import { Project, ProjectStatus } from '../models/project.js';
+import { Project, ProjectStatus } from '../models/project';
 
 // Project State, singleton, subscriber pattern
 type Listener<T> = (items: T[]) => void;
